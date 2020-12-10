5d4d6764898bba1c8ad2f1514682739a1936e998
Dinath <dinath22@live.fr>
2020-12-10 07:35:32 +0100

    Optimisation du CV

--- src/window/cv.rs
+++ src/window/cv.rs
@@ -21,98 +21,60 @@ macro_rules! clone {
         }
     );
 }
-
 pub fn create_cv(user: &Rc<RefCell<window::User>>) {
     let glade_src = include_str!("../glade/cv.glade");
     let builder = gtk::Builder::from_string(glade_src);
     let window: gtk::Window = builder.get_object("CV").unwrap();
 
-    let stack_work: Rc<RefCell<Vec<window::Work>>> = Rc::new(RefCell::new(Vec::new()));
-    let stack_school: Rc<RefCell<Vec<window::School>>> = Rc::new(RefCell::new(Vec::new()));
-    let stack_skill: Rc<RefCell<Vec<window::Skill>>> = Rc::new(RefCell::new(Vec::new()));
-    let stack_hobbie: Rc<RefCell<Vec<window::Hobbie>>> = Rc::new(RefCell::new(Vec::new()));
+    let stack_work: Rc<RefCell<Vec<(String, String, String, String)>>> = Rc::new(RefCell::new(Vec::new()));
+    let stack_school: Rc<RefCell<Vec<(String, String, String, String)>>> = Rc::new(RefCell::new(Vec::new()));
+    let stack_skill: Rc<RefCell<Vec<(String,String)>>> = Rc::new(RefCell::new(Vec::new()));
+    let stack_hobbie: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
 
-    let localization: gtk::Entry = builder.get_object("adress").unwrap();
-    let compl_adress: gtk::Entry = builder.get_object("compl_adress").unwrap();
-    let zipcode: gtk::Entry = builder.get_object("zipCode").unwrap();
-    let city: gtk::Entry = builder.get_object("city").unwrap();
-    let tel: gtk::Entry = builder.get_object("tel").unwrap();
-
-    let date_work: gtk::Entry = builder.get_object("dateWork").unwrap();
-    let company: gtk::Entry = builder.get_object("company").unwrap();
-    let job: gtk::Entry = builder.get_object("job").unwrap();
-    let description_work: gtk::Entry = builder.get_object("descriptionWork").unwrap();
     let add_work: gtk::Button = builder.get_object("addAdress").unwrap();
-
-    let date_school: gtk::Entry = builder.get_object("dateSchool").unwrap();
-    let university: gtk::Entry = builder.get_object("university").unwrap();
-    let field: gtk::Entry = builder.get_object("field").unwrap();
-    let description_school: gtk::Entry = builder.get_object("descriptionSchool").unwrap();
     let add_school: gtk::Button = builder.get_object("addSchool").unwrap();
-
-    let skill: gtk::Entry = builder.get_object("skill").unwrap();
-    let level: gtk::Entry = builder.get_object("level").unwrap();
     let add_skill: gtk::Button = builder.get_object("addSkill").unwrap();
-
-    let like: gtk::Entry = builder.get_object("hobbie").unwrap();
-    let add_hobbie: gtk::Button = builder.get_object("addHobbie").unwrap();
+    let add_hobbie : gtk::Button = builder.get_object("addHobbie").unwrap();
+    
+    let adress = window::Adress::build(builder.clone());
+    let work = window::Work::build(builder.clone());
+    let school = window::School::build(builder.clone());
+    let skill = window::Skill::build(builder.clone());
+    let hobbie = window::Hobbie::build(builder.clone());
 
     let validate: gtk::Button = builder.get_object("validate").unwrap();
 
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
 
     add_work.connect_clicked(clone!(stack_work => move |_| {
-        let wrk = window::Work {
-            date_work : date_work.get_text().to_string(),
-            company : company.get_text().to_string(),
-            job : job.get_text().to_string(),
-            description_work : description_work.get_text().to_string(),
-        };
-        stack_work.borrow_mut().push(wrk);
+        let work_string = work.to_string();
+        work.clear();
+        stack_work.borrow_mut().push(work_string);
     }));
 
     add_school.connect_clicked(clone!(stack_school => move |_| {
-        let sch = window::School {
-            date_school : date_school.get_text().to_string(),
-            university : university.get_text().to_string(),
-            field : field.get_text().to_string(),
-            description_school : description_school.get_text().to_string(),
-        };
-        stack_school.borrow_mut().push(sch);
+        let school_string = school.to_string();
+        school.clear();
+        stack_school.borrow_mut().push(school_string);
     }));
 
     add_skill.connect_clicked(clone!(stack_skill => move |_| {
-        let skl = window::Skill {
-            skill: skill.get_text().to_string(),
-            level: level.get_text().to_string(),
-        };
-        stack_skill.borrow_mut().push(skl);
+        let skill_string = skill.to_string();
+        skill.clear();
+        stack_skill.borrow_mut().push(skill_string);
     }));
 
     add_hobbie.connect_clicked(clone!(stack_hobbie => move |_| {
-        let hb = window::Hobbie {
-            like: like.get_text().to_string(),
-        };
-        stack_hobbie.borrow_mut().push(hb);
+        let hobbie_string = hobbie.to_string();
+        hobbie.clear();
+        stack_hobbie.borrow_mut().push(hobbie_string);
     }));
 
     validate.connect_clicked(
-        clone!(user,stack_work,stack_school,stack_skill,stack_hobbie => move |_| {
+        clone!(adress,user,stack_work,stack_school,stack_skill,stack_hobbie => move |_| {
 
-            let info = window::Adress {
-                localization : localization.get_text().to_string(),
-                compl_adress : compl_adress.get_text().to_string(),
-                zipcode : zipcode.get_text().to_string(),
-                city : city.get_text().to_string(),
-                tel : tel.get_text().to_string(),
-
-            };
             window::generate_cv::cv(
                 &user,
-                info,
+                &adress,
                 &stack_work,
                 &stack_school,
                 &stack_skill,
@@ -121,6 +83,11 @@ pub fn create_cv(user: &Rc<RefCell<window::User>>) {
     );
 
     window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
 
     gtk::main();
 }
--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,4 +1,5 @@
 extern crate gtk;
+use gtk::prelude::*;
 pub mod cv;
 pub mod generate_cv;
 pub mod menu;
@@ -18,32 +19,146 @@ pub struct User {
 }
 #[derive(Clone)]
 pub struct Adress {
-    localization: String,
-    compl_adress: String,
-    zipcode: String,
-    city: String,
-    tel: String,
+    localization: gtk::Entry,
+    compl_adress: gtk::Entry,
+    zipcode: gtk::Entry,
+    city: gtk::Entry,
+    tel: gtk::Entry,
 }
-
 #[derive(Clone)]
 pub struct Work {
-    date_work: String,
-    company: String,
-    job: String,
-    description_work: String,
+    date_work: gtk::Entry,
+    company: gtk::Entry,
+    job: gtk::Entry,
+    description_work: gtk::Entry,
 }
 #[derive(Clone)]
 pub struct School {
-    date_school: String,
-    university: String,
-    field: String,
-    description_school: String,
+    date_school: gtk::Entry,
+    university: gtk::Entry,
+    field: gtk::Entry,
+    description_school: gtk::Entry,
 }
 #[derive(Clone)]
 pub struct Skill {
-    skill: String,
-    level: String,
+    thing: gtk::Entry,
+    level: gtk::Entry,
 }
+#[derive(Clone)]
 pub struct Hobbie {
-    like: String,
+    like: gtk::Entry,
+}
+
+
+
+impl Adress {
+    ///Met dans un tupple les informations de l'utilisateur en String
+    fn to_string(&self) -> (String, String, String, String, String) {
+        let localization = self.localization.get_text().to_string();
+        let compl_adress = self.compl_adress.get_text().to_string();
+        let zipcode = self.zipcode.get_text().to_string();
+        let city = self.city.get_text().to_string();
+        let tel = self.tel.get_text().to_string();
+
+        return (localization, compl_adress, zipcode, city, tel);
+    }
+    
+    fn build(builder : gtk::Builder) -> Adress {
+        return Adress {
+            localization: builder.get_object("adress").unwrap(),
+            compl_adress: builder.get_object("compl_adress").unwrap(),
+            zipcode: builder.get_object("zipCode").unwrap(),
+            city: builder.get_object("city").unwrap(),
+            tel: builder.get_object("tel").unwrap(),
+        };
+    }
+}
+impl Work {
+    ///Met dans un tupple les informations de l'utilisateur en String
+    fn to_string(&self) -> (String, String, String, String) {
+        let date_work = self.date_work.get_text().to_string();
+        let company = self.company.get_text().to_string();
+        let job = self.job.get_text().to_string();
+        let description_work = self.description_work.get_text().to_string();
+
+        return (date_work, company, job, description_work);
+    }
+    fn build(builder : gtk::Builder) -> Work {
+        return Work {
+            date_work: builder.get_object("dateWork").unwrap(),
+            company: builder.get_object("company").unwrap(),
+            job: builder.get_object("job").unwrap(),
+            description_work: builder.get_object("descriptionWork").unwrap(),
+        };
+    }
+    ///Efface le texte entré
+    fn clear(&self){
+        self.date_work.set_text("");
+        self.company.set_text("");
+        self.job.set_text("");
+        self.description_work.set_text("");
+    }
+}
+
+impl School {
+    ///Met dans un tupple les informations de l'utilisateur en String
+    fn to_string(&self) -> (String,String,String,String) {
+        let date_school = self.date_school.get_text().to_string();
+        let university = self.university.get_text().to_string();
+        let field = self.field.get_text().to_string();
+        let description_school = self.description_school.get_text().to_string();
+
+        return (date_school,university,field,description_school);
+    }
+    fn build(builder : gtk::Builder) -> School {
+        return School {
+            date_school : builder.get_object("dateSchool").unwrap(),
+            university : builder.get_object("university").unwrap(),
+            field : builder.get_object("field").unwrap(),
+            description_school : builder.get_object("descriptionSchool").unwrap(),
+        };
+    }
+    ///Efface le texte entré
+     fn clear(&self){
+        self.date_school.set_text("");
+        self.university.set_text("");
+        self.field.set_text("");
+        self.description_school.set_text("");
+    }
+}
+impl Skill {
+    fn to_string(&self) -> (String,String) {
+        let thing = self.thing.get_text().to_string();
+        let level = self.level.get_text().to_string();
+
+        return (thing,level);
+    }
+    fn build(builder : gtk::Builder) -> Skill {
+        return Skill {
+            thing : builder.get_object("skill").unwrap(),
+            level : builder.get_object("level").unwrap(),
+        };
+    }
+    ///Efface le texte entré
+    fn clear(&self){
+        self.thing.set_text("");
+        self.level.set_text("");
+    }
+}
+impl Hobbie {
+    fn to_string(&self) -> String {
+        let like = self.like.get_text().to_string();
+
+        return like;
+    }
+    fn build(builder : gtk::Builder) -> Hobbie {
+        return Hobbie {
+            like : builder.get_object("hobbie").unwrap(),
+           
+        };
+    }
+    ///Efface le texte entré
+    fn clear(&self) {
+        self.like.set_text("");
+    }
 }
